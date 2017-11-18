600b6d6a659f1e46ca54f09732b89aaeb7cb8f83
Alex <agent.serega@yandex.ru>
2017-11-18 10:43:00 +0000

    small update#2

--- ArTanks/src/bot.cpp
+++ ArTanks/src/bot.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Alex on 18.11.2017.
-//
-
 #include "bot.h"
 
 Bot::Bot(Tank *val) : Player (val)
@@ -13,16 +9,7 @@ void Bot::genGeneration()
 {
     for(i = 0; i < POPULATION_SIZE; i++){//по всем особям поколения
 
-        build_person(f[i]); //сгенерировать особь нулевого поколения
-        write_person(f[i]); //записать ее в файл
-
-        generate_battle(x2, y2, x1, y1);
-
-        fitness[i] = results_scan(); //и посмотреть что вышло
-
     }
-
-    best_num = find_sum_and_max_fitness(fitness, f);
 
     generation++;
 }
